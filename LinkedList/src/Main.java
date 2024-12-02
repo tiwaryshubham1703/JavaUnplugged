3c4e2de6c7b79a06d60789e7cbdd26bf0b665e1f
Subham Kumar <141574421+tiwaryshubham1703@users.noreply.github.com>
2024-12-02 20:08:29 +0530

    Delete LinkedList directory

--- LinkedList/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Enter your choice :");
-        //need to write here menu based output for user interaction
-        //TODO
-    }
-}
